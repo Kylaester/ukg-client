94ff82764a1ee2c4e8d4eefe41f3fba65a76b417
Marven Pierre <marvenpierre@Marvens-Laptop.localdomain>
2023-05-05 11:51:06 -0400

    finish the header components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import './App.css';
+
 import Header from './components/Header/Header';
-// import FormOne from './components/FormOne/FormOne';
-// import Survey from './components/Survey/Survey';
 
 function App() {
   return (
     <>
     <Header/>
-    {/* <Survey /> */}
    
     </>
   );
