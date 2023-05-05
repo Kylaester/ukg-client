242711505f9bcd0efdce333d974a460102594468
GabrielaSernaBanks <gserna88@gmail.com>
2023-05-05 13:34:28 -0400

    Adding routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import Header from './components/Header/Header';
+import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
+
 // import FormOne from './components/FormOne/FormOne';
 // import Survey from './components/Survey/Survey';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/' element={<ThankYou/>}/>
+
+    
+    </Routes>
+
+    </BrowserRouter>
     <Header/>
     {/* <Survey /> */}
    
