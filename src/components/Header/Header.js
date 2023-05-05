b058d782ab6071de39ddfb760d6bc02e5496b1d3
Marven Pierre <marvenpierre@Marvens-Laptop.localdomain>
2023-05-05 09:09:09 -0400

    first commit with folder and styling

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import './Header.scss';
+import BackArrow from '../../asset/images/icons';
+import SearchIcon from '../../asset/images/icon/searchicon.jpg';
+
+
+
+function Header() {
+    return (
+        <>
+            <header>
+                <div>
+                <button>
+                    <img src={BackArrow}/>
+                </button>
+                <h2>Monthly Survey</h2>
+                </div>
+                <img src={SearchIcon}/>
+            </header>
+        </>
+    );
+}
+
+export default Header;
